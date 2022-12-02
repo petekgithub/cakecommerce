28a9c0c33af677978f6e57410b5caccff92cb0ce
Petech <petekshamitbeyli@gmail.com>
2022-12-02 22:51:44 +0300

    outlet-and-index

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home/home.component";
+
+const Navgation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I'm the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
 
 const Shop = () => {
   return <h1>Helloooooo</h1>;
@@ -8,8 +19,9 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
-        <Route path="/" element={<Shop />} />
+      <Route path="/" element={<Navgation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );
