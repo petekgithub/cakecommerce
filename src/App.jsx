afda9c77d872d25021dbe6e847a2375a55aa3d0a
Petech <petekshamitbeyli@gmail.com>
2022-12-02 20:26:27 +0300

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,12 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
 
-function App() {
-  const menus = [
-    {
-      id: 1,
-      title: "strawberry-brownie",
-      price: "£10",
-      imageUrl:
-        "https://images.pexels.com/photos/9652807/pexels-photo-9652807.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 2,
-      title: "blueberry-brownie",
-      price: "£12",
-      imageUrl:
-        "https://images.pexels.com/photos/5386662/pexels-photo-5386662.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 3,
-      title: "crunchy-brownie",
-      price: "£18",
-      imageUrl:
-        "https://images.pexels.com/photos/5386671/pexels-photo-5386671.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 4,
-      title: "cashew-brownie",
-      price: "£20",
-      imageUrl:
-        "https://images.pexels.com/photos/8820715/pexels-photo-8820715.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 5,
-      title: "banana-brownie",
-      price: "£15",
-      imageUrl:
-        "https://images.pexels.com/photos/6794152/pexels-photo-6794152.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-  ];
-
-  return <Directory menu={menus} />;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
 
 export default App;
